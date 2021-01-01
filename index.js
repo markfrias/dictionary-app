44d959f4093b87f261afe27468749659f8e95562
Mark Jabel Frias <friasmarkjabel@gmail.com>
2021-01-01 19:18:59 +0800

    Add data fetching feature to API

--- index.js
+++ index.js
@@ -3,21 +3,81 @@ const mysql = require('mysql')
 const app = express();
 const port = 8000;
 
+
 // Create a MySQL connection
 const con = mysql.createConnection({
    host: "localhost",
    user: "root",
-   password: ""
+   password: "",
+   database: "samplejs"
 });
+
+
+function requestNew() {
+  
+  
+  let data;
+  con.query("SELECT * FROM words", function (err, results) {
+      if (err) throw err;
+      console.log("Result: " + results);
+      data = results;
+      console.log("Data is " + data)
+        
+      })
+
+  return data;
+    
+
+  
+  
+  
+  
+}
+
+
+
+
+const fetchDummy = () => {
+  return "Dummy data";
+}
+
+
+
+
+
 
 con.connect(function(err) {
    if (err) throw err;
-console.log("Connected!");
+   console.log("Connected!");
+   
 });
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+  
 });
+
+// Fetch query results from database
+app.get('/data', (req, res) => {
+
+  let data;
+  con.query("SELECT * FROM words", function (err, results) {
+      if (err) throw err;
+      console.log("Result: " + results);
+      data = results;
+      console.log("Data is " + data)
+        
+      })
+  
+  setTimeout( () => {
+    res.send(data);
+
+  }, 2000)
+
+  
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
