532ee62b63921b9d0d79585f1a9a98bd8a81505d
Mark Jabel Frias <friasmarkjabel@gmail.com>
2021-01-01 14:49:46 +0800

    Add a git ignore file and add MySQL connection

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express')
+const mysql = require('mysql')
 const app = express();
 const port = 8000;
+
+// Create a MySQL connection
+const con = mysql.createConnection({
+   host: "localhost",
+   user: "root",
+   password: ""
+});
+
+con.connect(function(err) {
+   if (err) throw err;
+console.log("Connected!");
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
